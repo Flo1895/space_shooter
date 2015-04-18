46a7915f04360ff0ae714d8f16b5537a96100b4a
Florian Quinkert <florianquinkert@hotmail.com>
2015-04-18 17:19:41 +0200

    Added velocity value for bullets and set it to a random value in case of an enemy bullet

--- include/Bullet.h
+++ include/Bullet.h
@@ -11,6 +11,7 @@
 class Bullet : public GameObject {
 private:
   Direction direction;
+  int velocity;
 
 public:
   Bullet();
--- src/Bullet.cpp
+++ src/Bullet.cpp
@@ -9,8 +9,10 @@ Bullet::Bullet(int x, int y, sf::Texture *texture, Direction direction)
 : GameObject(x, y, texture) {
   if (direction == DOWN) {
   	this->type = "EnemyBullet";
+    this->velocity = Utils::getRandomNumber(200, 300);
   } else {
     this->type = "Bullet";
+    this->velocity = 250;
   }
   this->direction = direction;
 }
@@ -19,9 +21,9 @@ Bullet::~Bullet() {}
 
 void Bullet::move(float tickLength) {
   if (this->direction == UP) {
-    this->position.y -= tickLength*250;
+    this->position.y -= tickLength * this->velocity;
   } else {
-  	this->position.y += tickLength*250;
+  	this->position.y += tickLength * this->velocity;
   }
   this->sprite->setPosition(position);
   if (this->position.y < 0) {
